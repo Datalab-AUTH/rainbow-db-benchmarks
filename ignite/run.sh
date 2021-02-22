86d0ab9d48b44dbee299c1198625c23497a0bbc2
George Vlahavas <vlahavas@gmail.com>
2021-02-22 13:14:38 +0200

    Run both actions if any of the two is missing

--- ignite/run.sh
+++ ignite/run.sh
@@ -82,22 +82,24 @@ done
 sleep $(( $NODES * 20 ))
 
 for workload in a b c d e f; do
-	for action in load run; do
-		COMBINATION=$VARIANT-$workload-$NODES-${BANDWIDTH}Mbps-${NETWORK_DELAY}ms-$YCSB_OPERATION_COUNT-$YCSB_RECORD_COUNT-$YCSB_THREAD_COUNT-$action-$REPLICATION
-		OUTPUT_FILE=../output/$COMBINATION.out
-		if [ -f $OUTPUT_FILE ]; then
-			echo "Skipping $COMBINATION."
-		else
-			echo "Running YCSB $COMBINATION..."
-			docker exec ignite_ycsb_1 \
-				./bin/ycsb $action $VARIANT -p hosts=node1 \
-				-s -P ./workloads/workload$workload \
-				-p operationcount=$YCSB_OPERATION_COUNT \
-				-p recordcount=$YCSB_RECORD_COUNT \
-				-threads $YCSB_THREAD_COUNT | \
-				tee $OUTPUT_FILE
-		fi
-	done
+	COMBINATION=$VARIANT-$workload-$NODES-${BANDWIDTH}Mbps-${NETWORK_DELAY}ms-$YCSB_OPERATION_COUNT-$YCSB_RECORD_COUNT-$YCSB_THREAD_COUNT
+	if [ ! -f ${COMBINATION}-load-${REPLICATION}.out ] || [ ! -f ${COMBINATION}-run-${REPLICATION}.out ]; then
+		echo "Running YCSB $COMBINATION-$REPLICATION..."
+		docker exec ignite_ycsb_1 \
+			./bin/ycsb load $VARIANT -p hosts=node1 \
+			-s -P ./workloads/workload$workload \
+			-p operationcount=$YCSB_OPERATION_COUNT \
+			-p recordcount=$YCSB_RECORD_COUNT \
+			-threads $YCSB_THREAD_COUNT | \
+			tee ${COMBINATION}-load-${REPLICATION}.out
+		docker exec ignite_ycsb_1 \
+			./bin/ycsb run $VARIANT -p hosts=node1 \
+			-s -P ./workloads/workload$workload \
+			-p operationcount=$YCSB_OPERATION_COUNT \
+			-p recordcount=$YCSB_RECORD_COUNT \
+			-threads $YCSB_THREAD_COUNT | \
+			tee ${COMBINATION}-run-${REPLICATION}.out
+	fi
 done
 
 docker-compose -f $DOCKER_COMPOSE down
