e62576524f33afcdc89459e114e612d8ceb19c24
George Vlahavas <vlahavas@gmail.com>
2021-02-23 21:06:15 +0200

    Add check for ignite tests with wrong node number

--- output/check-outputs.sh
+++ output/check-outputs.sh
@@ -5,8 +5,24 @@ echo "====================="
 echo "Empty files: $EMPTY_FILES"
 echo "====================="
 find ./ -name "*.out" -size 0
+
 NO_RESULTS=`grep -L OVERALL *.out | wc -l`
 echo "==============================="
 echo "Files with no results: $NO_RESULTS"
 echo "==============================="
 grep -L OVERALL *.out
+
+N=0
+for nodes in 6 10 14 20; do
+	NEW=$( grep "Topology snap" ignite*-$nodes-* 2> /dev/null | \
+		grep -v "servers=$nodes" | \
+		sed "s/\(.*\)out:\(.*\)/\1out/" | wc -l )
+	N=$((N+NEW))
+done
+echo "=============================================="
+echo "Ignite files with wrong number of nodes: $N"
+echo "=============================================="
+	grep "Topology snap" ignite*-$nodes-* 2> /dev/null | \
+		grep -v "servers=$nodes" | \
+		sed "s/\(.*\)out:\(.*\)/\1out/"
+
